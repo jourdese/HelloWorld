30a583b82bdd4e3ad2c81eb0208270bc8af6765b
Jourdese Palacio <jourdesepalacio@pixelmine.org>
2025-08-26 14:39:16 +0800

    Initial Commit

---
+++ HelloWorld/HelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldApp.swift
+//  HelloWorld
+//
+//  Created by Jourdese Palacio on 8/26/25.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
