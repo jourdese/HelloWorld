b4e8715b505f878bdb1c3824a6bcffb6a886a9d1
Jourdese Palacio <jourdesepalacio@gmail.com>
2025-08-27 10:48:48 +0800

    AsyncImage: Basic, Scale, Placeholder

--- HelloWorld/ContentView.swift
+++ HelloWorld/ContentView.swift
@@ -8,14 +8,31 @@
 import SwiftUI
 
 struct ContentView: View {
+    private let imageURL: String = "https://credo.academy/credo-academy@3x.png"
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            // MARK: - 1. BASIC
+            //AsyncImage(url: URL(string: imageURL))
+            
+            // MARK: -2. SCALE
+            //AsyncImage(url: URL(string: imageURL), scale: 3.0)
+            
+            // MARK: -3. PLACEHOLDER
+            AsyncImage(url: URL(string:imageURL)) { image in
+                image
+                    .resizable()
+                    .scaledToFit()
+            } placeholder: {
+                Image(systemName: "photo.circle.fill")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(maxWidth: 128)
+                    .foregroundColor(.gray)
+                    .opacity(0.5)
+            }
+            .padding(40)
         }
-        .padding()
     }
 }
 
