676030a18faeb2bdee26485471fbad76a90ad06d
Jourdese Palacio <jourdesepalacio@gmail.com>
2025-08-27 12:08:55 +0800

    AsyncImage: Animation

--- HelloWorld/ContentView.swift
+++ HelloWorld/ContentView.swift
@@ -34,13 +34,16 @@ struct ContentView: View {
             //AsyncImage(url: URL(string: imageURL), scale: 3.0)
             
             // MARK: - 3. PLACEHOLDER
-//            AsyncImage(url: URL(string:imageURL)) { image in
-//                image.imageModifier()
-//            } placeholder: {
-//                Image(systemName: "photo.circle.fill").iconModifier()
-//            }
+        /*
+            AsyncImage(url: URL(string:imageURL)) { image in
+                image.imageModifier()
+            } placeholder: {
+                Image(systemName: "photo.circle.fill").iconModifier()
+            }
+         */
             
             // MARK: - 4. PHASE
+        /*
         AsyncImage(url: URL(string:imageURL)) { phase in
             //SUCCESS
             //FAILURE
@@ -52,6 +55,24 @@ struct ContentView: View {
                 Image(systemName: "ant.circle.fill").iconModifier()
             } else {
                 Image(systemName: "photo.circle.fill").iconModifier()
+            }
+        }
+        .padding(40)
+         */
+        
+            // MARK: 5. ANIMATION
+        AsyncImage(url: URL(string: imageURL), transaction: Transaction(animation: .spring(response: 0.5, dampingFraction: 0.6, blendDuration: 0.25))) {
+            phase in
+            switch phase {
+            case .success(let image):
+                image.imageModifier()
+                    .transition(.move(edge: .bottom))
+            case .failure(_):
+                Image(systemName: "ant.circle.fill").iconModifier()
+            case .empty:
+                Image(systemName: "photo.circle.fill").iconModifier()
+            @unknown default:
+                ProgressView()
             }
         }
         .padding(40)
